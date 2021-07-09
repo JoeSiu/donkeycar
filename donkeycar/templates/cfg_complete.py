060c2e2d1d97021307afdb30cabae222ae70502d
JoeSiu <2000.joesiu@gmail.com>
2021-07-09 17:10:33 +0800

    Added fps part

--- donkeycar/templates/cfg_complete.py
+++ donkeycar/templates/cfg_complete.py
@@ -349,3 +349,6 @@ REALSENSE_D435_ID = None        # serial number of camera or None if you only ha
 STOP_SIGN_DETECTOR = False
 STOP_SIGN_MIN_SCORE = 0.2
 STOP_SIGN_SHOW_BOUNDING_BOX = True
+
+# FPS counter
+SHOW_FPS = True
